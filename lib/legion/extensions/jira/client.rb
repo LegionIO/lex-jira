cf7c635dce827d7b8e3aac208382806d1f086d42
Esity <matthewdiverson@gmail.com>
2026-04-13 17:54:38 -0500

    integrate: wire all 27 runners into Client, remove old flat runners

--- lib/legion/extensions/jira/client.rb
+++ lib/legion/extensions/jira/client.rb
@@ -1,18 +1,49 @@
 # frozen_string_literal: true
 
-require_relative 'helpers/client'
-require_relative 'runners/issues'
-require_relative 'runners/projects'
-require_relative 'runners/boards'
-
 module Legion
   module Extensions
     module Jira
       class Client
         include Helpers::Client
-        include Runners::Issues
-        include Runners::Projects
-        include Runners::Boards
+
+        # Issues
+        include Issues::Runners::Issues
+        include Issues::Runners::Search
+        include Issues::Runners::Comments
+        include Issues::Runners::Transitions
+        include Issues::Runners::Attachments
+        include Issues::Runners::Worklogs
+        include Issues::Runners::Links
+        include Issues::Runners::RemoteLinks
+        include Issues::Runners::Votes
+        include Issues::Runners::Watchers
+        include Issues::Runners::Properties
+
+        # Projects
+        include Projects::Runners::Projects
+        include Projects::Runners::Components
+        include Projects::Runners::Versions
+        include Projects::Runners::Roles
+        include Projects::Runners::Categories
+
+        # Users, Groups, Permissions
+        include Users::Runners::Users
+        include Groups::Runners::Groups
+        include Permissions::Runners::Permissions
+
+        # Dashboards & Filters
+        include Dashboards::Runners::Dashboards
+        include Filters::Runners::Filters
+
+        # Agile
+        include Agile::Runners::Boards
+        include Agile::Runners::Sprints
+        include Agile::Runners::Epics
+        include Agile::Runners::Backlogs
+
+        # Admin
+        include Webhooks::Runners::Webhooks
+        include AuditRecords::Runners::AuditRecords
 
         attr_reader :opts
 
@@ -25,7 +56,11 @@ module Legion
         end
 
         def connection(**override)
-          super(**@opts, **override)
+          super(**@opts.merge(override))
+        end
+
+        def upload_connection(**override)
+          super(**@opts.merge(override))
         end
       end
     end
--- spec/legion/extensions/jira/client_spec.rb
+++ spec/legion/extensions/jira/client_spec.rb
@@ -34,4 +34,47 @@ RSpec.describe Legion::Extensions::Jira::Client do
       expect(client.connection).to be_a(Faraday::Connection)
     end
   end
+
+  describe '#upload_connection' do
+    it 'returns a Faraday connection' do
+      expect(client.upload_connection).to be_a(Faraday::Connection)
+    end
+  end
+
+  describe 'runner inclusion' do
+    it 'includes all 27 runner modules' do
+      expected_modules = [
+        Legion::Extensions::Jira::Issues::Runners::Issues,
+        Legion::Extensions::Jira::Issues::Runners::Search,
+        Legion::Extensions::Jira::Issues::Runners::Comments,
+        Legion::Extensions::Jira::Issues::Runners::Transitions,
+        Legion::Extensions::Jira::Issues::Runners::Attachments,
+        Legion::Extensions::Jira::Issues::Runners::Worklogs,
+        Legion::Extensions::Jira::Issues::Runners::Links,
+        Legion::Extensions::Jira::Issues::Runners::RemoteLinks,
+        Legion::Extensions::Jira::Issues::Runners::Votes,
+        Legion::Extensions::Jira::Issues::Runners::Watchers,
+        Legion::Extensions::Jira::Issues::Runners::Properties,
+        Legion::Extensions::Jira::Projects::Runners::Projects,
+        Legion::Extensions::Jira::Projects::Runners::Components,
+        Legion::Extensions::Jira::Projects::Runners::Versions,
+        Legion::Extensions::Jira::Projects::Runners::Roles,
+        Legion::Extensions::Jira::Projects::Runners::Categories,
+        Legion::Extensions::Jira::Users::Runners::Users,
+        Legion::Extensions::Jira::Groups::Runners::Groups,
+        Legion::Extensions::Jira::Permissions::Runners::Permissions,
+        Legion::Extensions::Jira::Dashboards::Runners::Dashboards,
+        Legion::Extensions::Jira::Filters::Runners::Filters,
+        Legion::Extensions::Jira::Agile::Runners::Boards,
+        Legion::Extensions::Jira::Agile::Runners::Sprints,
+        Legion::Extensions::Jira::Agile::Runners::Epics,
+        Legion::Extensions::Jira::Agile::Runners::Backlogs,
+        Legion::Extensions::Jira::Webhooks::Runners::Webhooks,
+        Legion::Extensions::Jira::AuditRecords::Runners::AuditRecords
+      ]
+      expected_modules.each do |mod|
+        expect(described_class.ancestors).to include(mod)
+      end
+    end
+  end
 end
